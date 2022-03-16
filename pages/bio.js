b6f8cba102585e4864c52c2376304ea4b71ef3a2
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-03-16 16:43:42 -0300

    Create bio.js

---
+++ pages/bio.js
@@ -0,0 +1,10 @@
+const Bio = () => {
+  return (
+    <div className="mt-3">
+      <p className="display-4 text-center">I'm Felipe Ferreira</p>
+      <p className="text-center">Felipe Ferreira's bio here</p>
+    </div>
+  )
+}
+
+export default Bio
