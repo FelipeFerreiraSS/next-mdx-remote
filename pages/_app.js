e982c70d75f00ecf9b81b02feacda2c68f5b7da8
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-03-16 16:27:10 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+      <Head>
+        <title>Next and MDX Blog</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+              integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+              crossOrigin="anonymous" />
+      </Head>
+
+      <div className="container">
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
+  )
+}                                                                                                           
 
 export default MyApp
