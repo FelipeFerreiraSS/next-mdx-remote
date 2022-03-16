0ec1f180849c0da7211cf73be8bc4451c2da921a
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-03-16 16:34:45 -0300

    Create Nav.jsx

---
+++ components/Nav.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+const Nav = () => {
+  return (
+    <nav className="nav p-3 border-bottom">
+      <Link href="/" passHref>
+        <h2 className="pointer">Felipe Ferreira</h2>
+      </Link>
+
+      <Link href="/bio" passHref>
+        <p className="ms-5 pointer lead my-auto">Bio</p>
+      </Link>
+    </nav>
+  )
+}
+
+export default Nav
