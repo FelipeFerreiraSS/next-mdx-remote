0260209b16dcf41165adb5d8e352381b9d9412ae
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-03-21 22:31:13 -0300

    ordenando posts por data

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,31 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
+// import fs from 'fs'
+// import path from 'path'
+// import matter from 'gray-matter'
 
-export default function Home({ posts }) {
+import { getSortedPostsData } from '../lib/posts'
+
+export default function Home({ allPostsData }) {
+  console.log(allPostsData)
   return (
     <div className="mt-5">
-      {posts.map((post, index) => (
+      {allPostsData.map((post, index) => (
         <Link href={'/blog/' + post.slug} passHref key={index}>
           <div className="card mb-3 pointer" style={{ maxWidth: '540px' }}>
             <div className="row g-0">
               <div className="col-md-8">
                 <div className="card-body">
-                  <h5 className="card-title">{post.frontMatter.title}</h5>
-                  <p className="card-text">{post.frontMatter.description}</p>
+                  <h5 className="card-title">{post.title}</h5>
+                  <p className="card-text">{post.description}</p>
                   <p className="card-text">
-                    <small className="text-muted">{post.frontMatter.date}</small>
+                    <small className="text-muted">{post.date}</small>
                   </p>
                 </div>
               </div>
               <div className="col-md-4 m-auto">
                 <Image
-                  src={post.frontMatter.thumbnailUrl}
+                  src={post.thumbnailUrl}
                   className="img-fluid mt-1 rounded-start"
                   alt="thumbnail"
                   width={500}
@@ -38,22 +41,31 @@ export default function Home({ posts }) {
   )
 }
 
-export const getStaticProps = async () => {
-  const files = fs.readdirSync(path.join('posts'))
+// export const getStaticProps = async () => {
+//   const files = fs.readdirSync(path.join('posts'))
 
-  const posts = files.map(filename => {
-    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
-    const { data: frontMatter } = matter(markdownWithMeta)
+//   const posts = files.map(filename => {
+//     const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
+//     const { data: frontMatter } = matter(markdownWithMeta)
 
-    return {
-      frontMatter,
-      slug: filename.split('.')[0]
-    }
-  })
+//     return {
+//       frontMatter,
+//       slug: filename.split('.')[0]
+//     }
+//   })
 
+//   return {
+//     props: {
+//       posts
+//     }
+//   }
+// }
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
   return {
     props: {
-      posts
+      allPostsData
     }
   }
 }
